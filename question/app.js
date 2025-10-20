97b5dd1cd06a67d0cfd0bf682f09a87d1898841b
sota-f625 <s7716xf625@gmail.com>
2025-10-20 16:19:42 +0900

    fix: セミコロン付け忘れ修正

--- question/app.js
+++ question/app.js
@@ -3,7 +3,7 @@
 
 let nickname = "藤村蒼太";
 let age = 30;
-let greet = "私のニックネームは" + nickname + "です。年齢は" + age + "です。"
+let greet = "私のニックネームは" + nickname + "です。年齢は" + age + "です。";
 
 console.log(greet);
 
