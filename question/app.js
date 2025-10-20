d48176b75ec562ba16fd485b7ca128cac60d0213
sota-f625 <s7716xf625@gmail.com>
2025-10-20 11:18:51 +0900

    add: Q4. 配列 × オブジェクト

--- question/app.js
+++ question/app.js
@@ -25,3 +25,26 @@ let user = {
 };
 
 console.log(user.age);
+
+
+// Q4. 配列 × オブジェクト
+
+let playerList = [
+  {
+    name: 'John',
+    age: 26,
+    favorites: ['Card Game', 'Basket Ball', 'Programming'],
+  },
+  {
+    name: 'Bob',
+    age: 33,
+    favorites: ['Tinder', 'The Legend of Zelda'],
+  },
+  {
+    name: 'Michael',
+    age: 22,
+    favorites: ['Football', 'Smash Bros.'],
+  },
+];
+
+console.log(playerList[1].favorites[1]);
